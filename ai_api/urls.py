fbd7935cbbf98a303188a38a67749d6fbd733ea5
vincentmuya <vincentmuya13@gmail.com>
2025-02-24 23:13:36 +0300

    unstaged changes before removing large files and virtual from git and adding .gitignore

--- ai_api/urls.py
+++ ai_api/urls.py
@@ -3,5 +3,5 @@ from .views import SimplePrediction, HousePricePrediction
 
 urlpatterns = [
     path('predict/', SimplePrediction.as_view(), name='simple-predict'),
-    path('predict_house_price/', HousePricePrediction.as_view(), name='house-price-predict'),
+    path('predict_house_price/', HousePricePrediction.as_view(), name='house-price-predict'), # http://127.0.0.1:8000/api/predict_house_price/?size=1600
 ]
